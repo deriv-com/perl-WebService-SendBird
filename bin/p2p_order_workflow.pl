d4dfcbfb232909125608a5396838957801666576
Kirill Sysoev <kirill@binary.com>
2020-03-03 07:44:54 +0000

    Adds sending notification to test channel

--- bin/p2p_order_workflow.pl
+++ bin/p2p_order_workflow.pl
@@ -7,55 +7,70 @@ use Data::Dumper;
 
 use Digest::MD5 qw(md5_hex);
 
-my ($app_id, $api_token, $action);
+my ($app_id, $api_token);
 
 GetOptions ("id=s" => \$app_id,
-            "token=s"   => \$api_token,
-            "action=s"  => \$action);
+            "token=s"   => \$api_token);
 
-$action //= 'create';
 
-die "One of the argument is missed: id, token, action" unless $app_id || $api_token || $action;
+die "One of the argument is missed: id, token" unless $app_id || $api_token;
 
 my $api = WebService::SendBird->new(app_id => $app_id, api_token => $api_token);
 
 my $uniq_postfix = md5_hex($$ .time) =~ s/\w{10}//r;
 
-if ($action eq 'create') {
-    my $client = $api->create_user(
-        user_id => 'client_' . $uniq_postfix,
-        nickname => 'Test Client',
-        profile_url => undef,
-    );
-    print "-----------------\n";
-    print 'Client user_id: ', $client->user_id, "\n";
+my $client = $api->create_user(
+    user_id => 'client_' . $uniq_postfix,
+    nickname => 'Test Client',
+    profile_url => undef,
+);
+print "-----------------\n";
+print 'Client user_id: ', $client->user_id, "\n";
 
-    my $client_token_data = $client->issue_session_token;
+my $client_token_data = $client->issue_session_token;
 
-    print 'Client session token: ', $client_token_data->{session_token}, "\n";
+print 'Client session token: ', $client_token_data->{session_token}, "\n";
 
-    my $advertiser = $api->create_user(
-        user_id => 'advertiser_' . $uniq_postfix,
-        nickname => 'Test Advertiser',
-        profile_url => undef,
-    );
+my $advertiser = $api->create_user(
+    user_id => 'advertiser_' . $uniq_postfix,
+    nickname => 'Test Advertiser',
+    profile_url => undef,
+);
 
-    print "-----------------\n";
-    print 'Advertiser user_id ', $advertiser->user_id, "\n";
+print "-----------------\n";
+print 'Advertiser user_id ', $advertiser->user_id, "\n";
 
-    my $advertiser_token_data = $advertiser->issue_session_token;
+my $advertiser_token_data = $advertiser->issue_session_token;
 
-    print 'Advertiser session token: ', $advertiser_token_data->{session_token}, "\n";
-    print "-----------------\n";
-
-
-    my $group_chat = $api->create_group_chat(
-        channel_url => 'order_chat_' . $uniq_postfix,
-        user_ids => [ $client->user_id, $advertiser->user_id ],
-        name => 'Chat for order ' . $uniq_postfix,
-    );
+print 'Advertiser session token: ', $advertiser_token_data->{session_token}, "\n";
+print "-----------------\n";
 
 
-    print 'Order Chat url: ', $group_chat->channel_url, "\n";
-    print "-----------------\n";
-}
+my $chat = $api->create_group_chat(
+    channel_url => 'order_chat_' . $uniq_postfix,
+    user_ids => [ $client->user_id, $advertiser->user_id ],
+    name => 'Chat for order ' . $uniq_postfix,
+);
+
+
+print 'Order Chat url: ', $chat->channel_url, "\n";
+print "-----------------\n";
+
+$chat->send_admin_message(
+    message => 'Order has benn created',
+    mentioned_user_ids => [ $advertiser->user_id ],
+);
+
+$chat->send_admin_message(
+    message => 'Order has been confirmed by ' . $advertiser->nickname,
+    mentioned_user_ids => [ $client->user_id ],
+);
+
+$chat->send_admin_message(
+    message => 'Order has been completed',
+    mentioned_user_ids => [ $client->user_id, $advertiser->user_id ],
+);
+
+print '3 notifications were added to channel ', $chat->channel_url, "\n";
+print "-----------------\n";
+
